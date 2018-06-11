56585e02cd44f424fbece764af66863949febae1
pmanlukas <lukas.pollmann@tum.de>
2018-06-11 16:33:32 +0200

    created consumer for api

--- api_consumer.py
+++ api_consumer.py
@@ -7,3 +7,14 @@ SPEC_PATH = "spec/test.json"
 #create payload
 specification = open(SPEC_PATH, "rb").read()
 payload = {"specOpenApi": specification}
+
+#create the request
+req = requests.post(API_URL, files=payload).json()
+
+#process the response and respond to the user
+if r["success"]:
+    for (i, result) in enumerate(r["predictions"]):
+        print("{}. {}: {:.4f}".format(i + 1, result["label"],
+            result["probability"]))
+else:
+    print("Error with Request")
