1abd36e7229fe84bf2cac6ca60c38e9f04c26140
pmanlukas <lukas.pollmann@tum.de>
2018-06-17 19:47:35 +0200

    refactor preprocessing function

--- classifier_api.py
+++ classifier_api.py
@@ -1,30 +1,57 @@
-#TODO: Add imports for keras
 #TODO: Add code to create openAPI spec of this api
 import numpy as np
+import pandas as pd
 import json
 import flask
 import io
+from collections import Counter
+from datetime import datetime
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Dense, Input, GlobalMaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+from keras.models import load_model
+
+
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
 model = None
+filepath = ""#TODO: add filepath of model
+
 
 #this method loads the pretrained model and its weights
 def load_model():
     #TODO: add code to load model and word embeddings
-    
+    model = load_model(filepath)
     #add creation of embedding layer here
     
     global model
     #add loading of model and weights here
 
+
 #this function is preprocessing the data
 def preprocess_data(spec):
-    processed_spec = []
+    #turn json into string
+	processed_spec = []
     strSpec = json.dumps(spec)
     processed_spec.append(strSpec)
    
-    return processed_spec
+    #tokenzie spec and generate a vector representation
+	tokenizer = Tokenizer(nb_words=35000)
+	tokenizer.fit_on_texts(processed_spec[0])
+	sequences = tokenizer.texts_to_sequences(processed_spec[0])
+
+	word_index = tokenizer.word_index
+	print('Found %s unique tokens.' % len(word_index))
+
+	#pad sequence to have necessary length for final model
+	data = pad_sequences(sequences, maxlen=75000)
+
+    return data
+
 
 #this function adds the endpoint for our api
 @app.route("/predict", methods=["POST"])
