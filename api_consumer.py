c12d5781103341aa2aabb81d354b6d3954a4416a
pmanlukas <lukas.pollmann@tum.de>
2018-06-22 08:57:46 +0200

    linted code

--- api_consumer.py
+++ api_consumer.py
@@ -1,20 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 
-#define URLs for Request and payload
-API_URL = ""
-SPEC_PATH = "spec/test.json"
+# define URLs for Request and payload
 
-#create payload
-specification = open(SPEC_PATH, "rb").read()
-payload = {"specOpenApi": specification}
+API_URL = ''
+SPEC_PATH = 'spec/test.json'
 
-#create the request
+# create payload
+
+specification = open(SPEC_PATH, 'rb').read()
+payload = {'specOpenApi': specification}
+
+# create the request
+
 req = requests.post(API_URL, files=payload).json()
 
-#process the response and respond to the user
-if r["success"]:
-    for (i, result) in enumerate(r["predictions"]):
-        print("{}. {}: {:.4f}".format(i + 1, result["label"],
-            result["probability"]))
+# process the response and respond to the user
+
+if r['success']:
+    for (i, result) in enumerate(r['predictions']):
+        print '{}. {}: {:.4f}'.format(i + 1, result['label'],
+                result['probability'])
 else:
-    print("Error with Request")
+    print 'Error with Request'
+
+
+			
